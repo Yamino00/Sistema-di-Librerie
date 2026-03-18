504110fd45e73a20809af0a06c7b10e1f6064924
fvleccese <federicoleccese335@gmail.com>
2026-03-18 11:37:29 +0100

    Aggiunta della logica per inviare i dati della libreria a Kafka dopo il salvataggio e gestione degli errori nel metodo save di LibreriaService

--- libreria/src/main/java/com/federico/libreria/service/LibreriaService.java
+++ libreria/src/main/java/com/federico/libreria/service/LibreriaService.java
@@ -1,23 +1,31 @@
 package com.federico.libreria.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.federico.libreria.dto.LibreriaDTO;
 import com.federico.libreria.entity.Libreria;
 import com.federico.libreria.mapper.LibreriaMapper;
 import com.federico.libreria.repository.LibreriaRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class LibreriaService {
 
     private final LibreriaRepository libreriaRepository;
     private final LibreriaMapper libreriaMapper;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
-    public LibreriaService(LibreriaRepository libreriaRepository, LibreriaMapper libreriaMapper) {
+    public LibreriaService(LibreriaRepository libreriaRepository, LibreriaMapper libreriaMapper, KafkaTemplate<String, String> kafkaTemplate) {
         this.libreriaRepository = libreriaRepository;
         this.libreriaMapper = libreriaMapper;
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = new ObjectMapper();
     }
 
     public List<LibreriaDTO> findAllLibreria() {
@@ -32,10 +40,19 @@ public class LibreriaService {
     }
 
     public LibreriaDTO save(LibreriaDTO nuovaLibreriaDTO) {
-        Libreria nuovaLibreria = libreriaMapper.toEntity(nuovaLibreriaDTO);
-        Libreria libreriaSalvata = libreriaRepository.save(nuovaLibreria);
-        return libreriaMapper.toDto(libreriaSalvata);
+        try {
+            Libreria nuovaLibreria = libreriaMapper.toEntity(nuovaLibreriaDTO);
+            Libreria libreriaSalvata = libreriaRepository.save(nuovaLibreria);
+            LibreriaDTO dtoSalvato = libreriaMapper.toDto(libreriaSalvata);
+            String payload = objectMapper.writeValueAsString(dtoSalvato);
+            kafkaTemplate.send("SyncElastic", payload);
+            return dtoSalvato;
+        } catch (Exception e) {
+            log.error("Errore durante la creazione della libreria: {}", e.getMessage());
+        }
+        return null;
     }
+
 
     public boolean existsById(Long id) {
         return libreriaRepository.existsById(id);
